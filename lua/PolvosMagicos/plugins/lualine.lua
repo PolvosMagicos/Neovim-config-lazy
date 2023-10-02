05fd6d119f758df1c0041800f8dc6025249eea7e
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2023-10-01 20:59:46 -0500

    feat: add configuration for c++ files using clangd and clang-format

--- lua/PolvosMagicos/plugins/lualine.lua
+++ lua/PolvosMagicos/plugins/lualine.lua
@@ -19,32 +19,32 @@ return {
 
 		local my_lualine_theme = {
 			inactive = {
-				a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+				a = { fg = colors.gray, bg = colors.outerbg },
 				b = { fg = colors.gray, bg = colors.outerbg },
 				c = { fg = colors.gray, bg = colors.innerbg },
 			},
 			visual = {
-				a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+				a = { fg = colors.darkgray, bg = colors.visual },
 				b = { fg = colors.gray, bg = colors.outerbg },
 				c = { fg = colors.gray, bg = colors.innerbg },
 			},
 			replace = {
-				a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+				a = { fg = colors.darkgray, bg = colors.replace },
 				b = { fg = colors.gray, bg = colors.outerbg },
 				c = { fg = colors.gray, bg = colors.innerbg },
 			},
 			normal = {
-				a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+				a = { fg = colors.darkgray, bg = colors.normal },
 				b = { fg = colors.gray, bg = colors.outerbg },
 				c = { fg = colors.gray, bg = colors.innerbg },
 			},
 			insert = {
-				a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+				a = { fg = colors.darkgray, bg = colors.insert },
 				b = { fg = colors.gray, bg = colors.outerbg },
 				c = { fg = colors.gray, bg = colors.innerbg },
 			},
 			command = {
-				a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+				a = { fg = colors.darkgray, bg = colors.command },
 				b = { fg = colors.gray, bg = colors.outerbg },
 				c = { fg = colors.gray, bg = colors.innerbg },
 			},
